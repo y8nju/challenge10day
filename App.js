daf629bbbd258d288cd3ffce5ff727cfe3a453c9
y8nju <89aruc@gmail.com>
2022-10-07 00:37:07 +0900

    Design: font, image, icon 변경

--- App.js
+++ App.js
@@ -5,7 +5,11 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+
 import { useFonts } from 'expo-font';
+import { Ionicons } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons'; 
+
 import TodoScreen from './screens/todo/todoScreen';
 import LoginScreen from './screens/account/loginScreen';
 import FeedScreen from './screens/feed/feedScreen';
@@ -18,86 +22,110 @@ const Drawer = createDrawerNavigator();
 
 
 function HomeStackNavigator() {
-  return (<Stack.Navigator initialRouteName="home" screenOptions={{
-    headerStyle: { backgroundColor: "#f2f2f2" },
-    headerTitleStyle: { fontFamily: "SUIT-Regular" },
-    animation: 'slide_from_right'
-  }}>
-    <Stack.Screen name="home" component={HomeScreen} />
-  </Stack.Navigator>)
+	return (<Stack.Navigator initialRouteName="home" screenOptions={{
+		headerStyle: { backgroundColor: "#f2f2f2" },
+		headerShadowVisible: false,
+		headerTitleStyle: { fontFamily: "Gmarket-Medium" },
+		animation: 'slide_from_right'
+	}}>
+		<Stack.Screen name="home" component={HomeScreen} />
+	</Stack.Navigator>)
 }
 function FeedStackNavigator() {
-  return (<Stack.Navigator initialRouteName="feed" screenOptions={{
-    headerStyle: { backgroundColor: "#f2f2f2" },
-    headerTitleStyle: { fontFamily: "SUIT-Regular" },
-    animation: 'slide_from_right'
-  }}>
-    <Stack.Screen name="feed" component={FeedScreen} />
-  </Stack.Navigator>)
+	return (<Stack.Navigator initialRouteName="feed" screenOptions={{
+		headerStyle: { backgroundColor: "#f2f2f2" },
+		headerShadowVisible: false,
+		headerTitleStyle: { fontFamily: "Gmarket-Medium" },
+		animation: 'slide_from_right'
+	}}>
+		<Stack.Screen name="feed" component={FeedScreen} />
+	</Stack.Navigator>)
 }
 function TodoDrawerNavigator() {
-  return (<Drawer.Navigator initialRouteName="todo" screenOptions={{
-    headerStyle: { backgroundColor: "#f2f2f2" },
-    headerTitleStyle: { fontFamily: "SUIT-Regular" },
-  }}>
-    <Drawer.Screen name="todo" component={TodoScreen} />
-  </Drawer.Navigator>)
+	return (<Drawer.Navigator initialRouteName="todo" screenOptions={{
+		headerStyle: { backgroundColor: "#f2f2f2" },
+		headerShadowVisible: false,
+		headerTitleStyle: { fontFamily: "Gmarket-Medium" },
+	}}>
+		<Drawer.Screen name="todo" component={TodoScreen} />
+	</Drawer.Navigator>)
 }
 function UserStackNavigator() {
-  return (<Stack.Navigator initialRouteName="userInfo" screenOptions={{
-    headerStyle: { backgroundColor: "#f2f2f2" },
-    headerTitleStyle: { fontFamily: "SUIT-Regular" },
-    animation: 'slide_from_right'
-  }}>
-    <Stack.Screen name="userInfo" component={UserInfoScreen} />
-  </Stack.Navigator>)
+	return (<Stack.Navigator initialRouteName="userInfo" screenOptions={{
+		headerStyle: { backgroundColor: "#f2f2f2" },
+		headerShadowVisible: false,
+		headerTitleStyle: { fontFamily: "Gmarket-Medium" },
+		animation: 'slide_from_right'
+	}}>
+		<Stack.Screen name="userInfo" component={UserInfoScreen} />
+	</Stack.Navigator>)
 }
 function GuestStackNavigator() {
-  <Stack.Navigator
-    initialRouteName="login" screenOptions={{
-      headerStyle: { backgroundColor: "#f2f2f2" },
-      headerTitleStyle: { fontFamily: "SUIT-Regular" },
-      animation: 'slide_from_right'
-    }}>
-    <Stack.Screen name="login" component={LoginScreen} />
-    {/* <Stack.Screen name="signUp" component={} /> */}
-  </Stack.Navigator>
+	<Stack.Navigator
+		initialRouteName="login" screenOptions={{
+			headerStyle: { backgroundColor: "#f2f2f2" },
+			headerShadowVisible: false,
+			headerTitleStyle: { fontFamily: "Gmarket-Medium" },
+			animation: 'slide_from_right'
+		}}>
+		<Stack.Screen name="login" component={LoginScreen} />
+		{/* <Stack.Screen name="signUp" component={} /> */}
+	</Stack.Navigator>
 }
 
 
 
 export default function App() {
-  const [fontLoaded] = useFonts({
-    'SUIT-Light': require('./assets/fonts/SUIT-Light.ttf'),	// 300
-    'SUIT-Regular': require('./assets/fonts/SUIT-Regular.ttf'),	// 400
-    'SUIT-SemiBold': require('./assets/fonts/SUIT-SemiBold.ttf'),	// 600
-    'SUIT-ExtraBold': require('./assets/fonts/SUIT-ExtraBold.ttf'),	// 800
-    'Kyobo': require('./assets/fonts/KyoboHandwriting.ttf'),
-  })
+	const [fontLoaded] = useFonts({
+		'Gmarket-Light': require('./assets/fonts/GmarketSansTTFLight.ttf'),
+		'Gmarket-Medium': require('./assets/fonts/GmarketSansTTFMedium.ttf'),
+		'Gmarket-Bold': require('./assets/fonts/GmarketSansTTFBold.ttf'),
+		'Diary': require('./assets/fonts/EF_Diary.ttf'),
+	})
 
-  if (!fontLoaded) {
-    return <></>
-  }
-  return (<>
-    <StatusBar style="auto" />
-    <NavigationContainer>
-      <Tab.Navigator screenOptions={{
-        tabBarLabelStyle: { fontFamily: 'SUIT-Regular' }
-      }}>
-        {/* challenge */}
-        <Tab.Screen name="HomeStack" component={HomeStackNavigator}
-          options={{ headerShown: false }} />
-        {/* feeed */}
-        <Tab.Screen name="FeedStack" component={FeedStackNavigator}
-          options={{ headerShown: false }} />
-        {/* todo < drawer */}
-        <Tab.Screen name="TodoDrawer" component={TodoDrawerNavigator}
-          options={{ headerShown: false }} />
-        {/* user */}
-        <Tab.Screen name="UserStack" component={UserStackNavigator}
-          options={{ headerShown: false }} />
-      </Tab.Navigator>
-    </NavigationContainer>
-  </>
-  );
+	if (!fontLoaded) {
+		return <></>
+	}
+	return (<>
+		<StatusBar style="auto" />
+		<NavigationContainer>
+			<Tab.Navigator screenOptions={{
+				tabBarLabelStyle: { fontFamily: 'Gmarket-Medium', paddingBottom: 5},
+				tabBarActiveTintColor: "#fb5438",
+				tabBarStyle: { backgroundColor: '#f2f2f2'}
+			}}>
+				{/* challenge */}
+				<Tab.Screen name="HomeStack" component={HomeStackNavigator} 
+					options={{headerShown: false,
+					title: '챌린지',
+					tabBarIcon:({ focused, color,  }) => (
+					// <MaterialCommunityIcons name={focused ? 'ballot' : 'ballot-outline' } color={color} size={24} />
+					<MaterialCommunityIcons name={focused ? 'card-bulleted' : 'card-bulleted-outline' } color={color} size={26} />
+				)}}/>
+				{/* feeed */}
+				<Tab.Screen name="FeedStack" component={FeedStackNavigator}
+					options={{ headerShown: false,
+						title: '피드',
+						tabBarIcon:({ focused, color,  }) => (
+						<MaterialCommunityIcons name={focused ? 'image-multiple' : 'image-multiple-outline' } color={color} size={focused ? 24 : 22} />
+					)}}/>
+				{/* todo < drawer */}
+				<Tab.Screen name="TodoDrawer" component={TodoDrawerNavigator}
+					options={{ headerShown: false,
+						title: '투두',
+						tabBarIcon:({ focused, color,  }) => (
+						// <MaterialCommunityIcons name={focused ? 'checkbox-marked' : 'checkbox-outline' } color={color} size={24} />
+						<MaterialCommunityIcons name={focused ? 'check-circle' : 'check-circle-outline' } color={color} size={24} />
+					)}}/>
+				{/* user */}
+				<Tab.Screen name="UserStack" component={UserStackNavigator}
+					options={{ headerShown: false,
+						title: '마이 페이지',
+						tabBarIcon:({ focused, color,  }) => (
+						<MaterialCommunityIcons name={focused ? 'account' : 'account-outline' } color={color} size={24} />
+					)}}/>
+			</Tab.Navigator>
+		</NavigationContainer>
+	</>
+	);
 }
--- components/customText.jsx
+++ components/customText.jsx
@@ -3,15 +3,15 @@ import { Text } from "react-native";
 export default function CustomText({ children, style, weight, type }) {
 	if (!type) {
 		if (weight === 300) {
-			return (<Text style={[{ fontFamily: 'SUIT-Light' }, style]}>{children}</Text>)
-		} else if (weight === 600) {
-			return (<Text style={[{ fontFamily: 'SUIT-SemiBold' }, style]}>{children}</Text>)
-		} else if (weight === 800) {
-			return (<Text style={[{ fontFamily: 'SUIT-ExtraBold' }, style]}>{children}</Text>)
+			return (<Text style={[{ fontFamily: 'Gmarket-Light' }, style]}>{children}</Text>)
+		} else if (weight === 700) {
+			return (<Text style={[{ fontFamily: 'Gmarket-Bold' }, style]}>{children}</Text>)
 		} else if (!weight) {
-			return (<Text style={[{ fontFamily: 'SUIT-Regular' }, style]}>{children}</Text>)
+			return (<Text style={[{ fontFamily: 'Gmarket-Medium' }, style]}>{children}</Text>)
 		}
 	} else if (type == 'hand') {
-		return (<Text style={[{ fontFamily: 'Kyobo' }, style]}>{children}</Text>)
+		return (<Text style={[{ fontFamily: 'Diary' }, style]}>{children}</Text>)
 	}
 }
+
+
